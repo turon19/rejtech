b6b5a0fb5ddaff789372bcf307cf82a331ac9b9a
Turon <axieniel19@gmail.com>
2025-12-05 08:27:14 +0800

    push config

---
+++ admin/includes/config.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = getenv("db_host");
+$username = getenv("db_username");
+$password = getenv("db_password");
+$name = getenv("db_name");
+$charset = "utf8mb4";
+
+$dsn = "mysql:host=$host;dbname=$name;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $username, $password, $options);
+} catch (PDOException $e) {
+    die('Connection failed ' . $e->getMessage());
+};
